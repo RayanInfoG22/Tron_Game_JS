c569f6f19dc7b58f30e7deb57558f73c831d014e
RayanInfoG22 <rayan.infor.g.2@gmail.com>
2025-11-13 17:19:16 +0100

    Changement

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 import Jeu from Jeu.js;
 import Grille from Grille.js;
+import Canvas from Canvas.js;
 
+
+let canvaElement=document.getElementsByClassName("canvas-container");
+canvaElement.
--- js/model/Canvas.js
+++ js/model/Canvas.js
@@ -12,3 +12,4 @@ class Canvas{
     }
 }
 
+export default Canvas;
